265d7597af6e0eae644d0695de1001586a86b37f
DennisYaksok <147363843+DennisYaksok@users.noreply.github.com>
2023-11-22 10:30:15 +0800

    Add files via upload

---
+++ lpu_web_application/ingig_market.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container"> 
+    <h1 style="text-align:center; margin-top:10px; font-size:70px; font-family:Algerian; color:black">Se-Ri`s Point of Sale</h1>
+        <div style="float:left">
+            <div>
+            <img src="perfume_images/burberry.jpg" data-toggle="tooltip" data-placement="bottom" title="Burberry" width="170" height="180" alt="Burberry" style="margin-top:30px; border:10px solid white; overflow:hidden; box-shadow: 0 0 5px; margin-bottom:30px ">
+            <img src="perfume_images/chanel.jpg" data-toggle="tooltip" data-placement="bottom" title="Chance Chanel" alt="Chance Chanel" width="170" height="180" style="margin-top:30px; border:10px solid white; overflow:hidden; box-shadow: 0 0 5px; margin-bottom:30px ">
+            <img src="perfume_images/coach.jpg" data-toggle="tooltip" data-placement="bottom" title="Coach" alt="Coach" width="170" height="180" style="margin-top:30px; border:10px solid white; overflow:hidden; box-shadow: 0 0 5px; margin-bottom:30px ">
+            <img src="perfume_images/dolce_gabana.jpg" data-toggle="tooltip" data-placement="bottom" title="Dolce and Gabana" alt="Dolce and Gabana" width="170" height="180" style="margin-top:30px; border:10px solid white; overflow:hidden; box-shadow: 0 0 5px; margin-bottom:30px ">
+            <img src="perfume_images/dune.jpg" data-toggle="tooltip" data-placement="bottom" title="Christian Dior" alt="Chrsitian Dior" width="170"height="180" style="margin-top:30px; border:10px solid white; overflow:hidden; box-shadow: 0 0 5px; margin-bottom:30px ">
+            <img src="perfume_images/gucci.jpg" data-toggle="tooltip" data-placement="bottom" title="Gucci Bloom" alt="Gucci Bloom" width="170" height="180" style="margin-top:30px; border:10px solid white; overflow:hidden; box-shadow: 0 0 5px; margin-bottom:30px ">
+            <img src="perfume_images/obsession.jpg" data-toggle="tooltip" data-placement="bottom" title="Calvin Klien" alt="Calvin Klien" width="170" height="180" style="margin-top:30px; border:10px solid white; overflow:hidden; box-shadow: 0 0 5px; margin-bottom:30px ">
+            <img src="perfume_images/versace.jpg" data-toggle="tooltip" data-placement="bottom" title="Versace" alt="Versace" width="170" height="180"style="margin-top:30px; border:10px solid white; overflow:hidden; box-shadow: 0 0 5px; margin-bottom:30px ">
+            <img src="perfume_images/zara.jpg" data-toggle="tooltip" data-placement="bottom" title="Zarra" alt="Zara" width="170" height="180" style="margin-top:30px;border:10px solid white; overflow:hidden; box-shadow: 0 0 5px; margin-bottom:30px ">
+            <img src="perfume_images/coco_chanel.jpg" data-toggle="tooltip" data-placement="bottom" title="Chanel" alt="Chanel" width="170" height="180" style="margin-top:30px; border:10px solid white; overflow:hidden; box-shadow: 0 0 5px; margin-bottom:30px ">
+            <img src="perfume_images/jo_malone.jpg" data-toggle="tooltip" data-placement="bottom" title="Jo Malone" alt="Jo Malone" width="170" height="180" style="margin-top:30px; border:10px solid white; overflow:hidden; box-shadow: 0 0 5px; margin-bottom:30px ">
+            <img src="perfume_images/bulgari.jpg" data-toggle="tooltip" data-placement="bottom" title="Bulgari" alt="Bulgari" width="170" height="180"style="margin-top:30px; border:10px solid white; overflow:hidden; box-shadow: 0 0 5px; margin-bottom:30px ">
+</div>
+<script>
+    $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+    });
+</script>
+ 
+</body>
+</html>
